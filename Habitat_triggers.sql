530788483d45ec231a52cb208a51553293e54e8f
xavierlei <xavier.c.f@hotmail.com>
2015-01-05 21:12:13 +0000

    bbb

--- Habitat_triggers.sql
+++ Habitat_triggers.sql
@@ -3,7 +3,6 @@ delimiter $$
 create procedure sp_actualizaMaterial(in matDescricao varchar(75), in quant int, in unit varchar(45))
 begin
 declare idMat int;
-declare erro bool default 0;
 		select id into idMat from Habitat.Material where Material.descricao = matDescricao
 					group by id;
 		if idMat is null then
@@ -54,3 +53,41 @@ create trigger tg_updateDoacao
 			end if;
 		 end if;
 end$$
+
+-- utilizadores
+
+
+
+CREATE USER 'admin'@'localhost' IDENTIFIED BY 'adminpassword';
+
+GRANT ALL ON Habitat.* TO 'admin'@'localhost';
+
+CREATE USER 'angariacao'@'localhost' IDENTIFIED BY 'angariacaopassword';
+
+GRANT SELECT ON Habitat.* TO 'angariacao'@'localhost';
+
+GRANT ALL ON Habitat.Eventos TO 'angariacao'@'localhost';
+GRANT ALL ON Habitat.Doacoes TO 'angariacao'@'localhost';
+GRANT ALL ON Habitat.Doadores TO 'angariacao'@'localhost';
+GRANT ALL ON Habitat.Voluntarios TO 'angariacao'@'localhost';
+GRANT ALL ON Habitat.VoluntariosEquipas TO 'angariacao'@'localhost';
+GRANT ALL ON Habitat.Equipas TO 'angariacao'@'localhost';
+
+CREATE USER 'familias'@'localhost' IDENTIFIED BY 'failiaspassword';
+
+GRANT SELECT ON Habitat.* TO 'familias'@'localhost';
+GRANT ALL ON Habitat.Familias TO 'familias'@'localhost';
+GRANT ALL ON Habitat.Elementos TO 'familias'@'localhost';
+GRANT ALL ON Habitat.Candidaturas TO 'familias'@'localhost';
+GRANT ALL ON Habitat.CandidaturaPerguntas TO 'familias'@'localhost';
+GRANT ALL ON Habitat.Perguntas TO 'familias'@'localhost';
+GRANT ALL ON Habitat.Anexos TO 'familias'@'localhost';
+
+CREATE USER 'obras'@'localhost' IDENTIFIED BY 'obraspassword';
+GRANT SELECT ON Habitat.* TO 'obras'@'localhost';
+GRANT ALL ON Habitat.Projectos TO 'obras'@'localhost';
+GRANT ALL ON Habitat.ProjectoTarefas TO 'obras'@'localhost';
+GRANT ALL ON Habitat.Tarefas TO 'obras'@'localhost';
+GRANT ALL ON Habitat.ProjectoMaterial TO 'obras'@'localhost';
+GRANT ALL ON Habitat.Material TO 'obras'@'localhost';
+GRANT ALL ON Habitat.ProjectoTarefaVoluntario TO 'obras'@'localhost';
